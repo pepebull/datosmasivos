e07015a2342269dfecd4e5fd394bae4c2fbb14d9
pepebull <60455910+pepebull@users.noreply.github.com>
2020-03-02 23:19:56 -0800

    P5Act Marcos Hernandez

--- Practices/P5.scala
+++ Practices/P5.scala
@@ -1,41 +1,44 @@
-//1. Returns a sort expression based on ascending order of the column.
-df.sort(asc("Sales"), desc("Company")).show()
+//1. Count the number of elements in the list which satisfy a predicate.
+df.select(count("Sales")).show()
 
-//2. Returns a sort expression based on the descending order of the column, and null values appear after non-null values.
-df.sort(asc("Company"), desc_nulls_last("Sales")).show()
-
-// 3. Aggregate function: returns the approximate number of distinct items in a group.
-df.agg(approx_count_distinct("Sales")).show()
-
-//5. Aggregate function: returns the kurtosis of the values in a group.
-df.select(kurtosis("Sales")).show()
-
-//6. Aggregate function: returns the last value of the column in a group.
+//2. Returns the last element of this list.
 df.select(last("Sales")).show()
 
-//7. Aggregate function: returns the average of the values in a group.
-df.select(mean("Sales")).show()
+//3. returns inverse cosine of columnName, as if computed by java.lang.Math.acos
+df.select(acos("Sales")).show()
 
-//8. Aggregate function: returns the skewness of the values in a group.
-df.select(skewness("Sales")).show()
+//4. Returns the first column that is not null, or null if all inputs are null.
+df.select(col("Sales")).show()
 
-//9. Aggregate function: alias for stddev_samp.
-df.select(stddev("Sales")).show()
+//5. Returns a Column based on the given column name. Alias of col.
+df.select(column("Sales")).show()
 
-//10. Aggregate function: returns the population standard deviation of the expression in a group.
-df.select(stddev_pop("Sales")).show()
+//6. Creates a Column of literal value.
+df.select(lit("Sales")).show()
 
-//11. Aggregate function: returns the sample standard deviation of the expression in a group.
-df.select(stddev_samp("Sales")).show()
+//7. Creates a new struct column that composes multiple input columns.
+df.select(struct("Sales")).show()
 
-//12. Aggregate function: returns the population variance of the values in a group.
-df.select(var_pop("Sales")).show()
+//8. Creates a Column of literal value.
+df.select(typedLit("Sales")).show()
 
-//13. Aggregate function: returns the unbiased variance of the values in a group.
-df.select(var_samp("Sales")).show()
+//9. Computes the cube-root of the given value
+df.select(cbrt("Sales")).show()
 
-//14. Aggregate function: alias for var_samp.
-df.select(variance("Sales")).show()
+//10. Computes the ceiling of the given value.
+df.select(ceil("Sales")).show()
 
-//15. Aggregate function: returns the average of the values in a group.
-df.select(avg("Sales")).show()
+//11. Returns a Column based on the given column name.
+df.select(collect_list("Sales")).show()
+
+//12. Aggregate function: returns a list of objects with duplicates.
+df.select(collect_list("Sales")).show()
+
+//13. Aggregate function: returns a set of objects with duplicate elements eliminated.
+df.select(collect_set("Sales")).show()
+
+//14. Aggregate function: returns the number of items in a group.
+df.select(count("Sales")).show()
+
+//15. Aggregate function: returns the number of distinct items in a group.
+df.select(countDistinct("Sales")).show()
